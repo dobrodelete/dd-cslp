7f5b2c96078e9c8d4988b21de927852be812e942
dobrodelete <kirkon3004@gmail.com>
2024-06-10 13:12:50 +0300

    add TaskTrackerNavbar.vue

--- frontend/components/task-tracker/TaskTrackerNavbar.vue
+++ frontend/components/task-tracker/TaskTrackerNavbar.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-800 text-white p-3 ">
     <ul class="flex space-x-4 justify-center">
       <nuxt-link to="/" class="px-3 hover:bg-gray-700 py-2 rounded">CSLP</nuxt-link>
-        <nuxt-link to="/task-tracker" class="px-3 hover:bg-gray-700 py-2 rounded">Домой</nuxt-link>
+        <nuxt-link to="/task-tracker" class="px-3 hover:bg-gray-700 py-2 rounded">Task Tracker</nuxt-link>
         <nuxt-link to="/task-tracker/settings" class="px-3 hover:bg-gray-700 py-2 rounded">Настройки</nuxt-link>
     </ul>
   </div>
